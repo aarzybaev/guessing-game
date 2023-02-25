2830386f68bb030acd7f791dd3528febdf42589a
Askhad <aarzybaev@gmail.com>
2023-02-25 11:29:39 +0600

    init:solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor () {
+        this.middle = 0;
+    }
 
-    setRange(min, max) {
-
+       setRange(min, max) {
+        this._min = min;
+        this._max = max;
     }
 
     guess() {
 
-    }
-
-    lower() {
-
+        this.middle = Math.round((this._max + this._min) / 2);
+        return this.middle;       
     }
 
     greater() {
+        this._min = this.middle++;
+        this._max = this._max--;
+        this.guess();
+    }
 
+    lower() {
+        this._min = this._min++;
+        this._max = this.middle--;    
+        this.guess();        
     }
 }
 
